2b79955d693b9bc8b2e2d5959e3c2700990c9845
madhuvarshinikumar <madhuvarshinikumar241@gmail.com>
2026-06-02 18:54:57 +0530

    Ready for deployment

--- app/database.py
+++ app/database.py
@@ -7,9 +7,24 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# Required for Neon PostgreSQL
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 from fastapi import FastAPI
-from app.routes.order import router as order_router
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import engine
 from app.models import Base
-from app.routes.order import router as order_router
 
-# routers
 from app.routes.auth import router as auth_router
 from app.routes.cart import router as cart_router
 from app.routes.product import router as product_router
 from app.routes.recipe import router as recipe_router
+from app.routes.order import router as order_router
 
-from fastapi.middleware.cors import CORSMiddleware
-# create app
 app = FastAPI()
 
-
-# create tables (DEV ONLY)
+# Create tables
 Base.metadata.create_all(bind=engine)
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,15 +23,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# register routers
+
+# Routers
 app.include_router(auth_router)
 app.include_router(cart_router)
 app.include_router(product_router)
 app.include_router(recipe_router)
 app.include_router(order_router)
 
-
-# home route
 @app.get("/")
 def home():
-    return {"message": "Backend Working"}
+    return {
+        "message": "Effortless Grocery Shop Backend Running"
+    }
